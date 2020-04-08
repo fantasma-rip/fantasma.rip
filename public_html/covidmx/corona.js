a7d1a48161d0407eec15c11f1467a56c64f17371
omar <adios@fantasma.rip>
2020-04-08 11:33:18 -0500

    viewport

--- public_html/covidmx/corona.js
+++ public_html/covidmx/corona.js
@@ -156,6 +156,14 @@
                         conseguirdatos(clave);
                     });
 
+                    
+                    let vh = window.innerHeight * 0.01;
+                    document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
 
 
                 });
