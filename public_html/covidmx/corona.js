7bd041bd150185f4e253dd51aa966043a83823ef
omar <adios@fantasma.rip>
2020-04-08 16:53:02 -0500

    zoomi

--- public_html/covidmx/corona.js
+++ public_html/covidmx/corona.js
@@ -1,174 +1,171 @@
+(function ($) {
+    $(window).on('load', function () {
 
-        (function ($) {
-                $(window).on('load', function () {
+        var datos = 'https://raw.githubusercontent.com/emirelesg/covid19-mx/master/public/api/stats.json';
 
-                    var datos = 'https://raw.githubusercontent.com/emirelesg/covid19-mx/master/public/api/stats.json';
+        var nombre = '';
+        var sospechosos = '';
+        var confirmados = '';
+        var muertes = '';
+        var clave = 'CDMX';
 
-                    var nombre = '';
-                    var sospechosos = '';
-                    var confirmados = '';
-                    var muertes = '';
-                    var clave = 'CDMX';
+        conseguirdatos(clave);
 
-                    conseguirdatos(clave);
+        var tenerdatos = '';
+        var error = '';
 
-                    var tenerdatos = '';
-                    var error = '';
-                    
 
-                    function conseguirdatos(clave) {
+        function conseguirdatos(clave) {
 
-                        tenerdatos = $.getJSON(datos, function (data) {
-                            $.each(data.statesAsArray, function (index, value) {
-                                if (value.key == clave) {
-                                    nombre = value.name;
+            tenerdatos = $.getJSON(datos, function (data) {
+                $.each(data.statesAsArray, function (index, value) {
+                    if (value.key == clave) {
+                        nombre = value.name;
+                        sospechosos = value.suspected;
+                        confirmados = value.confirmed;
+                        muertes = value.deaths;
+                        $('#sospechosos span').html(sospechosos);
+                        $('#infectados span').html(confirmados);
+                        $('#muertes span').html(muertes);
+                        $('#aqui').html(nombre);
+                        imprimir(nombre, sospechosos, confirmados,
+                            muertes);
+                    }
+                });
+            }).done(function () {
+                console.log("todo bien");
+            }).fail(function (tenerdatos, textStatus, error) {
+                var err = textStatus + ', ' + error;
+                console.log("fallo: " + err);
+            }).always(function () {
+                console.log("completo");
+            });
+
+        }
+        /*
+                            function sacargrafica() {
+                                $('.datos').html('');
+                                $.getJSON(datos, function (data) {
+                                    $.each(data.timeseries, function (index, value) {
+                                    fecha = value.date;
                                     sospechosos = value.suspected;
                                     confirmados = value.confirmed;
                                     muertes = value.deaths;
-                                    $('#sospechosos span').html(sospechosos);
-                                    $('#infectados span').html(confirmados);
-                                    $('#muertes span').html(muertes);
-                                    $('#aqui').html(nombre);
-                                    imprimir(nombre, sospechosos, confirmados,
-                                        muertes);
-                                }
-                            });
-                        }).done(function() {
-                            console.log("todo bien");
-                        }).fail(function(tenerdatos, textStatus, error) {
-                            var err = textStatus + ', ' + error;
-                            console.log("fallo: " + err);
-                        }).always(function() {
-                            console.log("completo");
-                        });
-
-                    }
-/*
-                    function sacargrafica() {
-                        $('.datos').html('');
-                        $.getJSON(datos, function (data) {
-                            $.each(data.timeseries, function (index, value) {
-                            fecha = value.date;
-                            sospechosos = value.suspected;
-                            confirmados = value.confirmed;
-                            muertes = value.deaths;
-                            var rconf = confirmados * 0.05;
-                            var rdead = (muertes / confirmados) * 100;
-                            rdead = rdead.toFixed(2);
-                            zinde = rconf.toFixed(0);
-                            $('.datos').append("<div class='estadogrph' style='height: " + rconf + "vw;'><span class='confirmed'>" + confirmados + "</span><span class='deaths'>" + muertes + "</span></div>");
-                            });
-                        });
-
-                    }
-                    */
-
-                    function imprimir(nombre, sospechosos, confirmados, muertes) {
-
-                        $('canvas').remove();
-                        var scene = new THREE.Scene();
-                        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window
-                            .innerHeight, 0.1, 1000);
-
-                        var renderer = new THREE.WebGLRenderer({
-                            alpha: true
-                        });
-                        renderer.setClearColor(0xE4E2DE, 0);
-                        renderer.setSize(window.innerWidth, window.innerHeight);
-                        container = document.getElementById( 'wrappi' );
-                        container.appendChild(renderer.domElement);
-
-                        window.addEventListener('resize', function () {
-                            var width = window.innerWidth;
-                            var height = window.innerHeight;
-                            renderer.setSize(width, height);
-                            camera.aspect = width / height;
-                            camera.updateProjectionMatrix();
-                        });
-                        controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-                        var light = new THREE.DirectionalLight(0xE4E2DE);
-                        light.position.set(0.2, 1, 1).normalize();
-                        scene.add(light);
-
-                        var R=20;
-                        for (let i = 0; i < sospechosos;) {
-                            if (i < muertes) {
-                                var material = new THREE.MeshToonMaterial({
-                                    color: 0xB72A14
+                                    var rconf = confirmados * 0.05;
+                                    var rdead = (muertes / confirmados) * 100;
+                                    rdead = rdead.toFixed(2);
+                                    zinde = rconf.toFixed(0);
+                                    $('.datos').append("<div class='estadogrph' style='height: " + rconf + "vw;'><span class='confirmed'>" + confirmados + "</span><span class='deaths'>" + muertes + "</span></div>");
+                                    });
                                 });
-                            } else if (i < confirmados && i > muertes) {
-                                var material = new THREE.MeshToonMaterial({
-                                    color: 0xAED4C2
-                                });
-                            } else {
-                                var material = new THREE.MeshToonMaterial({
-                                    color: 0x454545
-                                });
+
                             }
-                            if ($(window).width() < 960) {
-                            } else {
-                             }
+                            */
 
-                             var geometry = new THREE.SphereGeometry(0.2, 3, 3);
-                             var figura = new THREE.Mesh(geometry, material);
+        function imprimir(nombre, sospechosos, confirmados, muertes) {
 
-                             figura.position.x = ( Math.random() - 0.5) * R*2 * Math.random();
-                             figura.position.y = ( Math.random() - 0.5) * R*2 * Math.random() ;
-                             figura.position.z = ( Math.random() - 0.5) * R*2 * Math.random() ;
-                         
-                             figura.rotation.x = Math.random();
-                             figura.rotation.y = Math.random();
-                             figura.rotation.z = Math.random();
-                         
-                             var distance_squared = figura.position.x*figura.position.x + figura.position.y*figura.position.y + figura.position.z*figura.position.z;
-                         
-                             if(distance_squared <= R*R) {
-                                 scene.add(figura);
-                                 ++i;
-                             }
-                        }
+            $('canvas').remove();
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window
+                .innerHeight, 0.1, 1000);
 
-                        camera.position.z = 32;
+            var renderer = new THREE.WebGLRenderer({
+                alpha: true
+            });
+            renderer.setClearColor(0xE4E2DE, 0);
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            container = document.getElementById('wrappi');
+            container.appendChild(renderer.domElement);
 
-                        var update = function () {
-                            camera.position.z -= 0.001;
-                        };
-                        var render = function () {
-                            renderer.render(scene, camera);
-                        };
+            window.addEventListener('resize', function () {
+                var width = window.innerWidth;
+                var height = window.innerHeight;
+                renderer.setSize(width, height);
+                camera.aspect = width / height;
+                camera.updateProjectionMatrix();
+            });
+            controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-                        var GameLoop = function () {
-                            requestAnimationFrame(GameLoop);
-                            update();
-                            render();
-                        }
+            var light = new THREE.DirectionalLight(0xE4E2DE);
+            light.position.set(0.2, 1, 1).normalize();
+            scene.add(light);
 
-                        GameLoop();
-
-                    }
-
-
-
-
-
-                    $('.nextarr').click(function() {
-                        $('#estado option:selected').next().attr('selected', 'selected');
-                        var clave = $('#estado option:selected').val();
-                        conseguirdatos(clave);
+            var R = 20;
+            for (let i = 0; i < sospechosos;) {
+                if (i < muertes) {
+                    var material = new THREE.MeshToonMaterial({
+                        color: 0xB72A14
                     });
-                    $('.prevarr').click(function() {
-                        $('#estado option:selected').prev().attr('selected', 'selected');
-                        var clave = $('#estado option:selected').val();
-                        conseguirdatos(clave);
+                } else if (i < confirmados && i > muertes) {
+                    var material = new THREE.MeshToonMaterial({
+                        color: 0xAED4C2
                     });
-
-                    $('#estado').on('change', function () {
-                        var clave = $(this).val();
-                        conseguirdatos(clave);
+                } else {
+                    var material = new THREE.MeshToonMaterial({
+                        color: 0x454545
                     });
+                }
+                if ($(window).width() < 960) {} else {}
+
+                var geometry = new THREE.SphereGeometry(0.2, 5, 5);
+                var figura = new THREE.Mesh(geometry, material);
+
+                figura.position.x = (Math.random() - 0.5) * R * 2 * Math.random();
+                figura.position.y = (Math.random() - 0.5) * R * 2 * Math.random();
+                figura.position.z = (Math.random() - 0.5) * R * 2 * Math.random();
+
+                figura.rotation.x = Math.random();
+                figura.rotation.y = Math.random();
+                figura.rotation.z = Math.random();
+
+                var distance_squared = figura.position.x * figura.position.x + figura.position.y * figura.position.y + figura.position.z * figura.position.z;
+
+                if (distance_squared <= R * R) {
+                    scene.add(figura);
+                    ++i;
+                }
+            }
+
+            camera.position.z = 38;
+
+            var update = function () {
+                camera.position.z -= 0.01;
+            };
+            var render = function () {
+                renderer.render(scene, camera);
+            };
+
+            var GameLoop = function () {
+                requestAnimationFrame(GameLoop);
+                update();
+                render();
+            }
+
+            GameLoop();
+
+        }
 
 
 
-                });
-        })(jQuery);
+
+
+        $('.nextarr').click(function () {
+            $('#estado option:selected').next().attr('selected', 'selected');
+            var clave = $('#estado option:selected').val();
+            conseguirdatos(clave);
+        });
+        $('.prevarr').click(function () {
+            $('#estado option:selected').prev().attr('selected', 'selected');
+            var clave = $('#estado option:selected').val();
+            conseguirdatos(clave);
+        });
+
+        $('#estado').on('change', function () {
+            var clave = $(this).val();
+            conseguirdatos(clave);
+        });
+
+
+
+    });
+})(jQuery);
