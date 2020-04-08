698ce18d508e5b6525a33f0d93892c1502b6eb13
omar <adios@fantasma.rip>
2020-04-08 12:47:45 -0500

    wat

--- public_html/covidmx/corona.js
+++ public_html/covidmx/corona.js
@@ -157,13 +157,6 @@
                     });
 
                     
-                    let vh = window.innerHeight * 0.01;
-                    document.documentElement.style.setProperty('--vh', `${vh}px`);
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
 
 
                 });
