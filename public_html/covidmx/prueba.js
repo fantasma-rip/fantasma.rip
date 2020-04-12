63a946d2c4a46ba09d6c81f87f9729ffe31de273
omar <adios@fantasma.rip>
2020-04-12 02:07:56 -0500

    pruebawhite

--- public_html/covidmx/prueba.js
+++ public_html/covidmx/prueba.js
@@ -103,10 +103,10 @@
 
                 object = object.clone();
 
-                object.scale.set(15/muertes,15/muertes,15/muertes);
+                object.scale.set(5/muertes,5/muertes,5/muertes);
                 object.rotation.z = (Math.random() - 0.5) * R * 360 * Math.random();
                 var material = new THREE.MeshLambertMaterial({
-                    color: 0xc2c2ca
+                    color: 0xF7F7F8
                 });
 
                 object.traverse( ( obj ) => {
