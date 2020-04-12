25f0844e1202d3dd55094aeabebc753f9b6b4db4
omar <adios@fantasma.rip>
2020-04-12 02:09:08 -0500

    ambient light white

--- public_html/covidmx/prueba.js
+++ public_html/covidmx/prueba.js
@@ -74,7 +74,7 @@
         }
 
         function setupLights() {
-            let ambientLight = new THREE.AmbientLight(0x353535);
+            let ambientLight = new THREE.AmbientLight(0xF7F7F8);
             scene.add(ambientLight);
             scene.background = new THREE.Color( 0xF7F7F8 );
             let spotLight = new THREE.SpotLight(0xaaaaaa);
